789ad8d0024593f6637800c334f723c2ea75a386
window10_user <window10_user@DESKTOP-TG0TJN0>
2020-04-24 12:07:59 +0900

    userList1.jsp 구현

---
+++ WebContent/hw/jdbc1/userList1.jsp
@@ -0,0 +1,48 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page import="java.util.List, jsp_hw.jdbc1.*" %>
+<%
+List<User> userList = UserDAO.findAll();
+%>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <style>
+    div.container { width: 600px; margin: 50px auto; }
+    thead th { background-color: #eee; font-wight:bold;}
+    table{ border-collapse: collapse; width: 100%; }
+    td, th { padding: 4px; border: 1px solid lightgray; }
+    td:nth-child(4) { text-align: center; }
+  </style>
+</head>
+<body>
+<div class="container">
+
+  <h1>사용자목록</h1>
+  
+  <table class="table table-bordered table-condensed">
+      <thead>
+          <tr>
+              <th>사용자아이디</th>
+              <th>이름</th>
+              <th>이메일</th>
+              <th>학과</th>
+              <th>사용자유형</th>
+          </tr>
+      </thead>
+      <tbody>
+          <% for (User userData : userList) { %>
+              <tr>
+                  <td><%= userData.getUserid() %></td>
+                  <td><%= userData.getName() %></td>
+                  <td><%= userData.getEmail() %></td>
+                  <td><%= userData.getDepartmentName() %></td>
+                  <td><%= userData.getUserType() %></td>
+              </tr>
+          <% } %>
+      </tbody>
+  </table>
+
+</div>
+</body>
+</html>
---
+++ src/jsp_hw/jdbc1/UserDAO.java
@@ -0,0 +1,35 @@
+package jsp_hw.jdbc1;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO {
+	public static List<User> findAll() throws Exception {
+        String sql="SELECT user.* ,department.departmentName FROM user LEFT JOIN department ON user.departmentId=department.id";
+
+        try (Connection connection = DB.getConnection("student1");
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            ArrayList<User> resultList = new ArrayList<User>();
+            while (resultSet.next()) {
+            	User userData=new User();
+            	userData.setId(resultSet.getInt("id"));
+            	userData.setUserid(resultSet.getString("userid"));
+            	userData.setPassword(resultSet.getString("password"));
+            	userData.setName(resultSet.getString("name"));
+            	userData.setEmail(resultSet.getString("email"));
+            	userData.setDepartmentId(resultSet.getInt("departmentId"));
+            	userData.setDepartmentName(resultSet.getString("departmentName"));
+            	userData.setEnabled(resultSet.getBoolean("enabled"));
+            	userData.setUserType(resultSet.getString("userType"));
+
+                resultList.add(userData);
+            }
+            return resultList;
+        }
+    }
+}
