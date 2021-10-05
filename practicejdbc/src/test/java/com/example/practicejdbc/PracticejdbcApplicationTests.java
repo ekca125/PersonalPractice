441bf156145e3fd9dba76fb966d46b23bb8fa185
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-10-05 16:22:29 +0900

    commit

---
+++ practicejdbc/src/test/java/com/example/practicejdbc/PracticejdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicejdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticejdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
